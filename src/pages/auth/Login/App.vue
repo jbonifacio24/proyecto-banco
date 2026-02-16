883bdab2a9500dd0804fdb599bbfcd155b331a86
Kenny Gonzales <31164836+K3nnyGC@users.noreply.github.com>
2026-02-16 16:36:37 -0500

    Login y dashboard

---
+++ src/pages/auth/Login/App.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import LoginForm from './parts/LoginForm.vue';
+
+export default defineComponent({
+    name: "LandingApp",
+    components: {
+        LoginForm
+    }
+})
+</script>
+
+<template>
+    <div class="font-display bg-slate-900 text-slate-100 h-screen overflow-hidden flex flex-col md:flex-row">
+        <LoginForm />
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/pages/private/main/dashboard/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Dashboard from './parts/Dashboard.vue';
+
+export default defineComponent({
+    name: "DashboardApp",
+    components: {
+        Dashboard
+    }
+})
+</script>
+
+<template>
+    <div
+        class="bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark min-h-screen flex flex-col transition-colors duration-200">
+        <Dashboard />
+    </div>
+</template>
+
+<style scoped></style>

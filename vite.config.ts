e4e0021370a28876f73918072ff3b0bbad63ea7f
jbonifacio24 <34145244+jbonifacio24@users.noreply.github.com>
2026-02-17 15:14:36 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/proyecto-banco/',
+  base: process.env.NODE_ENV === 'production' ? '/proyecto-banco/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
